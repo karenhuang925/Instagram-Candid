11c92ec9926ba633ed26f1c861d86ca2577e8835
George Merida <gmerida92@gmail.com>
2022-11-19 13:32:56 -0800

    Post Route in Progress

--- Candid/app/api/post_routes.py
+++ Candid/app/api/post_routes.py
@@ -1,19 +1,24 @@
 from flask import Blueprint, jsonify
+from sqlalchemy.orm import relationship, sessionmaker, joinedload, load_only
 from ..models.like import Post
 
 post_routes = Blueprint('posts', __name__)
 
 # Get all Posts
-@post_routes.route('/')
+@post_routes.route('/posts')
 def all_posts():
-    pass
+    post = Post.query.get(id).options(joinedload(Post.medias).options(load_only('id', 'type', 'mediaFile')), joinedload(Post.user).options(load_only('id','profileName', 'previewImage')))
+    return {'Posts': jsonify(post)}
 
 # Get all Posts of Users Followed by Current User
 # Get all Posts created by the Current User
+
 # Get details of a Post from an id
-@post_routes.route('/<int:id>', methods=["GET"])
+@post_routes.route('/posts/<int:id>', methods=["GET"])
 def get_post_by_id(id):
-    pass
+    post = Post.query.get(id).options(joinedload(Post.medias).options(load_only('id', 'type', 'mediaFile')), joinedload(Post.user).options(load_only('id','profileName', 'previewImage')))
+    return jsonify(post)
+
 # Create a Post
 # Edit a Post
 # Delete a Post
