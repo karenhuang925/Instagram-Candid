d6ac8119ec009422ba19393a94223d6c3d6215f0
George Merida <gmerida92@gmail.com>
2022-11-19 15:34:42 -0800

    Seeder testing in progress

--- Candid/app/models/like.py
+++ Candid/app/models/like.py
@@ -37,7 +37,7 @@ class Post(Like):
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
-    id = db.Column(db.Integer, db.ForeignKey('likes.id'), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     caption = db.Column(db.String(256))
     location = db.Column(db.String(100))
