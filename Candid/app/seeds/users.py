a558e4c94f93c8a41a3198c3c6eb91ea5d093437
Kelly <kelly.a.artola@ricealumni.net>
2022-11-19 23:30:32 -0600

    adding likeable id to liked seeders

--- Candid/app/seeds/users.py
+++ Candid/app/seeds/users.py
@@ -3,38 +3,13 @@ from app.models import db, User, environment, SCHEMA
 
 # Adds a demo user, you can add other users here if you want
 def seed_users():
-
-    # user1 = User(first_name = "Whindersson",
-    #             last_name = "Nunes",
-    #             username = "whindersonnunes",
-    #             biography = "biography 1",
-    #             email = "whindersonnunes@gmail.com",
-    #             password = "password1234",
-    #             preview_image = "https://upload.wikimedia.org/wikipedia/commons/f/f6/Whindersson_Nunes_em_2019.png"
-    #             )
-    # user2 = User(first_name = "Huda",
-    #             last_name = "Kattan",
-    #             username = "hudabeauty",
-    #             biography = "biography 2",
-    #             email = "hudabeauty@gmail.com",
-    #             password = "password1234",
-    #             preview_image = "https://image.cnbcfm.com/api/v1/image/106371890-1580835090681huda-3.jpg"
-    #             )
-
-    # user3 = User(first_name = "Eleanora",
-    #             last_name = "Pons",
-    #             username = "lelepons",
-    #             biography = "biography 3",
-    #             email = "lelepons@gmail.com",
-    #             password = "password1234",
-    #             preview_image = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Lele_Pons_2019_by_Glenn_Francis.jpg/1200px-Lele_Pons_2019_by_Glenn_Francis.jpg"
-    #             )
-
     objects = [
+        User(first_name = "Whindersson", last_name = "Nunes", username = "whindersonnunes", biography = "biography 1", email = "whindersonnunes@gmail.com", password = "password1234", preview_image = "https://upload.wikimedia.org/wikipedia/commons/f/f6/Whindersson_Nunes_em_2019.png"),
+        User(first_name = "Huda", last_name = "Kattan", username = "hudabeauty", biography = "biography 2", email = "hudabeauty@gmail.com", password = "password1234", preview_image = "https://image.cnbcfm.com/api/v1/image/106371890-1580835090681huda-3.jpg"),
+        User(first_name = "Eleanora", last_name = "Pons", username = "lelepons", biography = "biography 3", email = "lelepons@gmail.com", password = "password1234", preview_image = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Lele_Pons_2019_by_Glenn_Francis.jpg/1200px-Lele_Pons_2019_by_Glenn_Francis.jpg"),
         User(first_name = "Nusret", last_name = "Gokce", username = "nusr_et", biography = "biography 4", email = "nusr_et@gmail.com", password = "password1234", preview_image = "https://m.media-amazon.com/images/M/MV5BZWMyMTJlMjMtODgyYy00YmIzLWE4ZjQtMWMyZGY0NzZhNDQyXkEyXkFqcGdeQXVyNDg4MjkzNDk@._V1_.jpg"),
         User(first_name = "Dan", last_name = "Blizerian", username = "danblizerian", biography = "biography 5", email = "danblizerian@gmail.com", password = "password1234", preview_image = "https://upload.wikimedia.org/wikipedia/commons/1/1c/Dan_Bilzerian.jpg")
     ]
-    # db.session.add(user2)
     db.session.bulk_save_objects(objects)
     db.session.commit()
 
