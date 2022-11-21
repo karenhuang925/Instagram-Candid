4edac45748d3b01e27b020af6cc564f4db2c70df
George Merida <gmerida92@gmail.com>
2022-11-20 17:03:50 -0800

    Post routes in progress

--- Candid/app/api/post_routes.py
+++ Candid/app/api/post_routes.py
@@ -1,23 +1,96 @@
-# from flask import Blueprint, jsonify
-# from sqlalchemy.orm import relationship, sessionmaker, joinedload, load_only
-# from ..models.like import Post
+from flask import Blueprint, jsonify
+from sqlalchemy.orm import relationship, sessionmaker, joinedload, load_only
+from ..models.post import Post
+from ..models.user import User
 
-# post_routes = Blueprint('posts', __name__)
+post_routes = Blueprint('posts', __name__)
 
 # Get all Posts
-# @post_routes.route('/posts')
-# def all_posts():
-#     post = Post.query.get(id).options(joinedload(Post.medias).options(load_only('id', 'type', 'mediaFile')), joinedload(Post.user).options(load_only('id','profileName', 'previewImage')))
-#     return {'Posts': jsonify(post)}
+@post_routes.route('/posts')
+def all_posts():
 
-# # Get all Posts of Users Followed by Current User
-# # Get all Posts created by the Current User
+    posts = Post.query.options(joinedload(Post.medias).options(load_only('id', 'user_id', 'type', 'media_file'))).all()
+    return {
+        "Posts" : [
+            {
+                "id": post.id,
+                "userId": post.user_id,
+                "caption": post.caption,
+                "location": post.location,
+                "created_at": post.created_at,
+                "updated_at": post.updated_at,
+                "Media" :[
+                    {
+                        "id": media.id,
+                        "user_id": media.user_id,
+                        "media_file": media.media_file,
+                        "type": media.type
+                    } for media in post.medias
+                ]
+            } for post in posts
+        ]
+    }
 
-# # Get details of a Post from an id
-# @post_routes.route('/posts/<int:id>', methods=["GET"])
-# def get_post_by_id(id):
-#     post = Post.query.get(id).options(joinedload(Post.medias).options(load_only('id', 'type', 'mediaFile')), joinedload(Post.user).options(load_only('id','profileName', 'previewImage')))
-#     return jsonify(post)
+# Get all Posts created by the Current User
+@post_routes.route('/users/<int:id>/posts', methods=["GET"])
+def get_posts_by_current_user(id):
+
+    # current_user = User.query.filter(id == User.id).first()
+    posts = Post.query.filter(id == Post.user_id).options(joinedload(Post.medias).options(load_only('id', 'user_id', 'type', 'media_file'))).all()
+    return {
+        "Posts" : [
+            {
+                "id": post.id,
+                "userId": post.user_id,
+                "caption": post.caption,
+                "location": post.location,
+                "created_at": post.created_at,
+                "updated_at": post.updated_at,
+                "Media" :[
+                    {
+                        "id": media.id,
+                        "user_id": media.user_id,
+                        "media_file": media.media_file,
+                        "type": media.type
+                    } for media in post.medias
+                ]
+            } for post in posts
+        ]
+    }
+
+
+# Get all Posts of Users Followed by Current User
+@post_routes.route('/users/<int:id>/follows/posts')
+    # Need find all the people the current user follows
+    # Need to find the posts of the 
+    pass
+
+# Get details of a Post from an id
+@post_routes.route('/posts/<int:id>', methods=["GET"])
+def get_post_by_id(id):
+
+    post = Post.query.filter(id == Post.id).options(joinedload(Post.medias).options(load_only('id','user_id', 'type', 'media_file')), joinedload(Post.user).options(load_only('id','username', 'preview_image'))).first()
+    return {
+        "id": post.id,
+        "userId": post.user_id,
+        "caption": post.caption,
+        "location": post.location,
+        "created_at": post.created_at,
+        "updated_at": post.updated_at,
+        "Media" : [
+            {
+                "id": media.id,
+                "user_id": media.user_id,
+                "media_file": media.media_file,
+                "type": media.type
+            } for media in post.medias
+                ],
+        "Owner": {
+            "id": post.user.id,
+            "username": post.user.username,
+            "previewImage": post.user.preview_image
+        }
+    }
 
 # Create a Post
 # Edit a Post
