782ee6d51f4d456ee2d80592faae44d2ccd74df5
George Merida <gmerida92@gmail.com>
2022-12-04 16:22:38 -0800

    Post Feed in progress

--- react-app/src/components/Posts/Post/FeedPost/FeedPostComponents/ImageComponent/ImageScroll.js
+++ react-app/src/components/Posts/Post/FeedPost/FeedPostComponents/ImageComponent/ImageScroll.js
@@ -4,40 +4,36 @@ import { useState, useEffect } from 'react'
 
 
 function ImageComponent({ images }) {
-    // let imageState = {
-    //     currentIndex: 0,
-    //     movement: 0,
-    // }
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const length = images.length;
 
-    const [currentIndex, setCurrentIndex] = useState(0)
-    const [movement, setMovement] = useState(0)
+    const nextImage = () => {
+        setCurrentIndex(currentIndex === length - 1 ? 0 : currentIndex + 1)
+    }
 
-    // const handleMovement = (delta) => {
+    const prevImage = () => {
+        setCurrentIndex(currentIndex === 0 ? length - 1 : currentIndex - 1)
+    }
 
-    // }
-
-    // const handleWheel = (e) => {
-    //     handleMovement(e.deltaX)
-    // }
-
-    // onWheel={handleWheel}
-
-
-
+    if (!Array.isArray(images) || images.length <= 0) return null
 
     return (
         <div className='main-image-container'>
             <div className='image-swiper'>
-                <i className="fa-solid fa-house"></i>
-                {/* <i className="fa-regular fa-circle-arrow-left fa-2x"></i> */}
-                {images.map((image) => {
+                <div className='right-arrow' onClick={nextImage}><i className="fa-solid fa-rotate-right fa-2x"></i></div>
+                <div className='left-arrow' onClick={prevImage}><i class="fa-solid fa-rotate-left fa-2x"></i></div>
+                {images.map((image, index) => {
                     return (
-                        <img
-                            className='post-media'
-                            src={image.media_file}
-                            key={image.id}
-                            alt={null}
-                        />
+                        <div>
+                            {index === currentIndex && (
+                                <img
+                                    className='post-media'
+                                    src={image.media_file}
+                                    key={image.id}
+                                    alt='aPicture'
+                                />
+                            )}
+                        </div>
                     )
                 })}
             </div>
