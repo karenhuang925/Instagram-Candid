2df1fc6f0f07d88f8a3545a8e4c8608ad78d7977
karenhuang925 <92420431+karenhuang925@users.noreply.github.com>
2022-12-05 19:51:18 -0800

    add imagecomp and userdetail

--- react-app/src/components/Posts/PostDetail/PostDetail.js
+++ react-app/src/components/Posts/PostDetail/PostDetail.js
@@ -1,12 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import {useDispatch, useSelector} from 'react-redux'
 import './PostDetail.css'
+import ImageComponent from '../Post/FeedPost/FeedPostComponents/ImageComponent'
+import { loadPostById } from '../../../store/singlepost';
 
-function PostDetail() {
+function PostDetail({postId}) {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(loadPostById(postId))
+    }, [dispatch])
+
+    let post = useSelector((state) => state.singlePost.post)
+
+    if (!post) {return null;}
 
     return (
         <section className='modal-outer'>
-            <div className='image'></div>
-            <div className='info'></div>
+            <div className='image'>
+                <div className='imageComp'>
+                    <ImageComponent images={post.Media} ></ImageComponent>
+                </div>
+            </div>
+            <div className='info'>
+                <div className='userinfo'>
+                    <img alt='preview' src={post.Owner.previewImage} className='profile-pic'></img>
+                    <div class='usernameAndLocation'>
+                        <p className='username'>{post.Owner.username}</p>
+                        <p>{post.location}</p>
+                    </div>
+                </div>
+                <div className='postinfo'>
+                    <div className='captionAndComments'></div>
+                    <div className='actionButton'></div>
+                    <div className='addComment'></div>
+                </div>
+            </div>
         </section>
     );
 }
